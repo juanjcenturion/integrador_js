74a23851c2230d92e9e6c8d34efff6bd6fccab82
Juan Jose Centurion <j.centurion@itecriocuarto.org.ar>
2023-10-05 12:18:40 -0300

    Menu_finish

--- src/components/SubMenu/SubMenu.jsx
+++ src/components/SubMenu/SubMenu.jsx
@@ -11,8 +11,7 @@ function SubMenu({ menuItems, idFirstNivel, activeMenuItem, openSubMenus, handle
       cursor: "pointer"
     };
 
-    const arrow = menuItem.isFolder ? (openSubMenus.includes(menuItem.id) ? 'v' : '>') : null;
-
+    
     const nivel1Items = menuItems.filter(item => item.idPadre === null || item.idPadre === idFirstNivel);
     const nivel2Items = menuItems.filter(item => {return nivel1Items.some(nivel1Item => nivel1Item.id === item.idPadre);});
     const nivel3Items = menuItems.filter(item => {return nivel2Items.some(nivel2Item => nivel2Item.id === item.idPadre);});
@@ -25,6 +24,15 @@ function SubMenu({ menuItems, idFirstNivel, activeMenuItem, openSubMenus, handle
       itemClassName = 'nivel-3-item';
     }
 
+    const arrow = menuItem.isFolder ? (openSubMenus.includes(menuItem.id) ? ( <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='arrow-right' viewBox="0 0 16 16">
+        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+      </svg>
+    )
+    : (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" className='arrow-down' fill="currentColor" viewBox="0 0 16 16">
+        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+      </svg>)
+    ) : null;
+
     return (
       <a
         href='/#'
@@ -33,7 +41,11 @@ function SubMenu({ menuItems, idFirstNivel, activeMenuItem, openSubMenus, handle
         className={itemClassName}
         onClick={() => handleMenuItemClick(menuItem.id)}
       >
-      <p>{menuItem.name} {arrow}</p>
+      <p>
+        {menuItem.name}
+        <span className='flecha'>{arrow}</span> 
+      </p>
+      
       </a>
     );
   }
